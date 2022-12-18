394167ffc30dafe242096d382059eb96d21e3046
alikazmi <akazmi20@gmail.com>
2022-12-18 18:01:38 +0500

    no code altered only package-loc updated and gitignore updated with .env file

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,14 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 require("dotenv").config();
-console.log(process.env.INFURIA_KEY)
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: process.env.INFURIA_URL,
-      accounts: [process.env.INFURIA_KEY],
+      url: process.env.CHAIN_URL,
+      accounts: [process.env.PRIVATE_KEY],
     }
   }
 };
